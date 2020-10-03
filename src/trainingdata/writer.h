0e69a903159d16e13ca2a3d0e05749b2684ceaa5
Samuel Sonning <samuel.sonning@gmail.com>
2020-10-03 22:26:38 +0200

    flag for folder

--- src/trainingdata/writer.h
+++ src/trainingdata/writer.h
@@ -38,7 +38,7 @@ class TrainingDataWriter {
  public:
   // Creates a new file to write in data directory. It will has @game_id
   // somewhere in the filename.
-  TrainingDataWriter(int game_id);
+  TrainingDataWriter(int game_id, std::string folder = "");
   TrainingDataWriter(std::string filename);
 
   ~TrainingDataWriter() {
